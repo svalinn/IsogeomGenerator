d896ffea7871daf08b7a6b4164cb6548eb1fa941
Kalin Kiesling <krkiesling@gmail.com>
2018-02-08 11:55:29 -0600

    made lots of changes and never committed whoops

--- yt/ww-yt.py
+++ yt/ww-yt.py
@@ -1,15 +1,32 @@
 import yt
+import sys
 
-mesh = yt.load("../magic/mesh-ww-tags.h5m")
 
-min_ww, min_loc = mesh.find_min("ww_n")
-max_ww, max_loc = mesh.find_max("ww_n")
+def load_mesh(f):
+    mesh = yt.load(f)
+    return mesh
 
-print(mesh.find_field_values_at_point("ww_n", [0.,0.,0.]))
+def do_other_stuff(mesh):
+    min_ww, min_loc = mesh.find_min("ww_n")
+    max_ww, max_loc = mesh.find_max("ww_n")
 
-dd = mesh.all_data()
-dd.extract_isocontours("ww_n", 0.002, "test.obj", True)
-#print(dd.quantities)
-#print(mesh.field_list)
+    print(mesh.find_field_values_at_point("ww_n", [0.,0.,0.]))
 
-#mesh.surface(mesh, 'ww_n', 0.1)
+    dd = mesh.all_data()
+    dd.extract_isocontours("ww_n", 0.002, "test.obj", True)
+    #print(dd.quantities)
+    #print(mesh.field_list)
+
+    #mesh.surface(mesh, 'ww_n', 0.1)
+
+
+def main()
+
+    f = sys.argv[1]
+    mesh = load_mesh(f)
+    do_other_stuff(mesh)
+
+
+
+if __name__ == "__main__":
+    main()
