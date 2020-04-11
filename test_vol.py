c53bc49beadfe2731dd2d104c6cbb99795be8d10
Kalin Kiesling <krkiesling@gmail.com>
2020-04-11 16:01:10 -0500

    moved to test folder, simple first test

--- test_vol.py
+++
@@ -1,8 +0,0 @@
-import IsogeomGenerator as iso
-import unittest
-
-
-class TestIsogeom(unittest.TestCase):
-
-    def test_sample(self):
-        assert(True)
---
+++ tests/test_vol.py
@@ -0,0 +1,14 @@
+from IsogeomGenerator import vol
+import unittest
+
+
+class TestIsogeom(unittest.TestCase):
+
+    def test_assign_levels(self):
+
+        v = vol.IsoVolume()
+        levels = [0.1, 0.2, 0.05]
+        exp = sorted(levels)
+
+        v.assign_levels(levels)
+        assert(inst.levels == exp)
