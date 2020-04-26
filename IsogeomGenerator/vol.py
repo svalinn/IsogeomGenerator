78ec07749a1c06153fcf3f6125fc45e42466eaac
Kalin Kiesling <krkiesling@gmail.com>
2020-04-26 16:34:01 -0500

    starting version for all possible user arguments.

--- IsogeomGenerator/vol.py
+++ IsogeomGenerator/vol.py
@@ -15,11 +15,11 @@ def parse_arguments():
     """parse user args
     """
 
-    parser = argparse.ArgumentParser(description="Generate isosurface geometry with VisIt and MOAB")
+    parser = argparse.ArgumentParser(description="Generate isosurface geometry from a Cartesian mesh with VisIt and MOAB")
 
     # select mode: start-to-finsih, VisIt only, MOAB only
     parser.add_argument('-m', '--mode',
-                        action = 'store_const',
+                        action = 'store',
                         nargs = 1,
                         choices = ['full', 'visit', 'moab'],
                         default = 'full',
@@ -36,9 +36,10 @@ def parse_arguments():
                         )
 
     parser.add_argument('-f', '--filename',
-                        action = 'store_const',
+                        action = 'store',
                         nargs = 1,
                         required = False,
+                        default = None,
                         metavar = 'MESH_FILENAME',
                         dest = 'mesh_file',
                         type = str,
@@ -47,9 +48,10 @@ def parse_arguments():
                         )
 
     parser.add_argument('-d', '--data',
-                        action = 'store_const',
+                        action = 'store',
                         nargs = 1,
                         required = False,
+                        default = None,
                         metavar = 'NAME',
                         dest = 'data',
                         type = str,
@@ -62,7 +64,8 @@ def parse_arguments():
                         action = 'store',
                         nargs = '+',
                         required = False,
-                        metavar = 'val1 val2 ...',
+                        default = None,
+                        metavar = 'VAL1 VAL2 ...',
                         dest = 'levels',
                         type = float,
                         help = 'List of values used to generate isosurfaces in VisIt. ' +\
@@ -70,15 +73,181 @@ def parse_arguments():
                             'Only required for moab-only mode if levels are not provided in a file.'
                         )
     parser.add_argument('-lf', '--levelfile',
-                        action = 'store_const',
+                        action = 'store',
                         nargs = 1,
                         required = False,
+                        default = None,
                         metavar = 'LEVELS_FILE',
                         dest = 'level_file',
                         type = str,
                         help = 'Relative path to file containing values to use for the isosurface levels. ' +\
                             'File should be structured to have one value (float) per line.'
                         )
+    parser.add_argument('-gl', '--generatelevels',
+                        action = 'store',
+                        nargs = 1,
+                        choices = [None, 'ratio', 'log', 'lin'],
+                        required = False,
+                        default = None,
+                        metavar = 'LEVEL_GEN_MODE',
+                        dest = 'levelgenmode',
+                        type = str,
+                        help = 'If set, specifies the mode for generating ' +\
+                            'level values to be used for the isosurfaces in ' +\
+                            'full mode or visit-only mode.\n' +\
+                            'If used, values for the min level (-lmin), ' +\
+                            'max level (-lmax), and the ratio or number of levels (-N) ' +\
+                            'are also required.\n' +\
+                            'Only required if level values are not assigned (-lv) or provided in a file (-lf). \n' +\
+                            'Options:\n' +\
+                            '    ratio: N is the ratio between levels ranging from the min value upto, but not exceeding, the max value.\n' +\
+                            '    log: N is the number of levels to be evenly spaced logarithmically between the min and max values.\n' +\
+                            '    log: N is the number of levels to be evenly spaced linearly between the min and max values.'
+                        )
+
+    parser.add_argument('-lmin', '--levelmin',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = None,
+                        metavar = 'MIN_VAL',
+                        dest = 'minN',
+                        type = float,
+                        help = 'float, minimum value to use for generating set ' +\
+                            'of level values to be used for the isosurfaces. ' +\
+                            'Only required if generating levels for full mode or visit-only mode. ' +\
+                            'If used, must also provide a maximum value (-lmax).'
+                        )
+
+    parser.add_argument('-lmax', '--levelmax',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = None,
+                        metavar = 'MAX_VAL',
+                        dest = 'maxN',
+                        type = float,
+                        help = 'float, maximum value to use for generating set ' +\
+                            'of level values to be used for the isosurfaces. ' +\
+                            'Only required if generating levels for full mode or visit-only mode. ' +\
+                            'If used, must also provide a minimum value (-lmin).'
+                        )
+
+    parser.add_argument('-N', '--numlevels',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = None,
+                        metavar = 'N',
+                        dest = 'N',
+                        type = float,
+                        help = 'If generating levels (-gl), it is either ' +\
+                            'the ratio between adjacent level values (ratio mode), ' +\
+                            'or the number of levels to generate (log or lin mode).'
+                        )
+
+    parser.add_argument('-db', '--database',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = "/tmp",
+                        metavar = 'PATH',
+                        dest = 'db',
+                        type = str,
+                        help = 'Relative path to folder for isosurface meshfiles. ' +\
+                            'In full mode, it is where isosurface files will written ' +\
+                            'to from Visit and read from for creating the geometry. ' +\
+                            'In visit only mode, this is where files will be written. ' +\
+                            'In moab only mode, this is where files will be read from. ' +\
+                            'Default is a folder named tmp/ in the current directory.'
+                        )
+
+    parser.add_argument('-m', '--mergetol',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = 1e-5,
+                        metavar = 'MERGE_TOL',
+                        dest = mergetol,
+                        type = float,
+                        help = 'Merge tolerance for mesh based merging of ' +\
+                            'coincident surfaces. Default=1e-5.'
+                        )
+
+    parser.add_argument('-n', '--norm',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = 1,
+                        metavar = 'NORM_FACTOR',
+                        dest = norm,
+                        type = float,
+                        help = 'All level values will be multiplied by this normalization factor ' +\
+                            'when the geometry is generated. Default=1'
+                        )
+
+    parser.add_argument('-v', '--viz',
+                        action = 'store_true',
+                        required = False,
+                        dest = 'tagviz',
+                        help = 'If set, surfaces generated in full or moab-only mode ' +\
+                            'will be tagged with data for visualization purposes.'
+                        )
+
+    parser.add_argument('-wl', '--writelevels',
+                        action = 'store_true',
+                        required = False,
+                        dest = 'writelevels',
+                        help = 'If set, values used for generating isosurfaces ' +\
+                            'in full or visit-only mode will be written to a file named levels.txt ' +\
+                            'in the database folder. ' +\
+                            'File can then be read in with the --levelfile argument.'
+                        )
+
+    parser.add_argument('-tv', '--tagval',
+                        action = 'append',
+                        nargs = 1,
+                        required = False,
+                        default = None,
+                        metavar = 'TAG_VAL',
+                        dest = tagvals,
+                        type = float,
+                        help = 'Use with --tagname. Value will be assigned to ' +\
+                            'a MOAB tag on the geometry whose name corresponds to --tagname specified. ' +\
+                            'Option can be used multiple times for multiple tags. ' +\
+                            'The number of times this option is used must match the number of times --tagname is used.'
+                        )
+
+    parser.add_argument('-tn', '--tagname',
+                        action = 'append',
+                        nargs = 1,
+                        required = False,
+                        default = None,
+                        metavar = 'TAG_NAME',
+                        dest = tagnames,
+                        type = str,
+                        help = 'Use with --tagval. Name will be used to create ' +\
+                            'a MOAB tag on the geometry with this name whose value ' +\
+                            'is the corresponding --tagval. ' +\
+                            'Option can be used multiple times for multiple tags. ' +\
+                            'The number of times this option is used must match the number of times --tagval is used.'
+                        )
+
+    parser.add_argument('-g', '--geomfile',
+                        action = 'store',
+                        nargs = 1,
+                        required = False,
+                        default = None,
+                        metavar = 'GEOM_FILENAME',
+                        dest = geomfile,
+                        type = str,
+                        help = 'Filename and relative path to write generated isosurface geometry file. ' +\
+                            'Must be either a .h5m or .vtk file name. Default name is geom-DATA.h5m ' +\
+                            'where DATA is the name of data specified by -d if provided. ' +\
+                            'If not provided, then default is geom.h5m',
+                            'Default location is the database location (-db).'
+                        )
+
 
 class IsoVolume(object):
     """This class contains methods to create a DAGMC geometry of
