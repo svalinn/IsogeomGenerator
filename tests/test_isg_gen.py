d322ef392247e7098564eb976b06155c43ef661e
Kalin Kiesling <krkiesling@gmail.com>
2020-07-01 14:18:48 -0500

    start of test file for parent clasS

---
+++ tests/test_isg_gen.py
@@ -0,0 +1,39 @@
+# test parent class
+
+import os
+import pytest
+
+from IsogeomGenerator import isg_gen
+
+levels = [1.0, 2.0, 5.0, 3.0]
+levels_exp = [1.0, 2.0, 3.0, 5.0]
+data = 'ww'
+db = test_dir = os.getcwd() + "/tests/test_files/"
+
+
+def test_init_none():
+    r0 = r1 = r2 = False
+    ig = isg_gen.IsoGeomGen()
+    if ig.levels is None:
+        r0 = True
+    if ig.data is None:
+        r1 = True
+    if ig.db is None:
+        r2 = True
+    assert(all([r0, r1, r2]))
+
+
+def test_init_input():
+    r0 = r1 = r2 = False
+    ig = isg_gen.IsoGeomGen(levels=levels, data=data, db=db)
+    if ig.levels == levels_exp:
+        r0 = True
+    if ig.data == data:
+        r1 = True
+    if ig.db == db:
+        r2 = True
+    assert(all([r0, r1, r2]))
+
+
+def test_init_levels_error():
+    pass
