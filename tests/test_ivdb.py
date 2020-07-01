a9863452ce11de98b76d46f9404bbf32ea090750
Kalin Kiesling <krkiesling@gmail.com>
2020-07-01 16:08:47 -0500

    temp test to play with super

--- tests/test_ivdb.py
+++ tests/test_ivdb.py
@@ -7,5 +7,7 @@ from IsogeomGenerator import ivdb
 
 
 def test_init():
-    iv = ivdb.IvDb()
-    assert(iv.levels is None)
+    iv = ivdb.IvDb(levels=[1, 2], data='ww')
+    assert(iv.levels == [1., 2.])
+    assert(iv.data == 'ww')
+    assert(iv.db is None)
